ed591ca72fb9e87012a9a8fdfcc9bce8fb619cb7
cutz-j <cutz309@gmail.com>
2018-11-20 23:03:15 +0900

    Update

--- nlp.py
+++ nlp.py
@@ -17,18 +17,26 @@ def noun(wordList):
     res = []
     twit = Twitter()
     for i, j in wordList:
+        tmp_dict = {}
+        tmp_list = []
+        tmp_list_body = []
         for k, l in twit.pos(i, norm=True, stem=True):
-            tmp_list = []
-            if l == 'Noun' and len(k) > 1:
-                tmp_list.append(k)
+            if l == 'Noun' or l =='Adjective':
+                if len(k) > 1:
+                    tmp_list.append(k)
         for k, l in twit.pos(j, norm=True, stem=True):
-            if l == 'Noun' and len(k) > 1:
-                tmp_list.append(k)
-        res.append(tmp_list)
+            if l == 'Noun' or l =='Adjective':
+                if len(k) > 1:
+                    tmp_list_body.append(k)
+        tmp_dict['header'] = tmp_list
+        tmp_dict['body'] = tmp_list_body
+        res.append(tmp_dict)
     return res
-
-## 빈도 분석 ##
 
 if __name__ == "__main__":
     h_noun = noun(h_art)
     j_noun = noun(j_art)
+    h = pd.DataFrame(h_noun)
+    h.to_excel("d:/news_project/h_noun.xlsx", header=None, index=None, encoding='utf-8')
+    j = pd.DataFrame(j_noun)
+    j.to_excel("d:/news_project/j_noun.xlsx", header=None, index=None, encoding='utf-8')
