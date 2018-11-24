19fef67d04db3c5637391467ba1361bc78610fc6
cutz-j <cutz309@gmail.com>
2018-11-24 17:49:48 +0900

    Update w2v.py

--- w2v.py
+++ w2v.py
@@ -4,6 +4,7 @@ import pandas as pd
 from gensim.models import word2vec as w2v
 import multiprocessing
 import os
+import sklearn.manifold
 
 ## load_excel -> 한겨레, 중앙 ##
 os.chdir("d:/news_project/")
@@ -18,17 +19,34 @@ j_noun = np.array(j_noun)
 h_noun = np.array(h_noun)
 
 ## parameter ##
-num_features = 300
-min_word_count = 3
-num_workers = multiprocessing.cpu_count()
-window_size = 5
-seed = 7
-downsampling = 1e-3
+model = None
 
-sample = h_noun[0,1].split(",")
+def word2vector(noun, word_list):
+    global model
+    num_features = int(len(noun) / 2)
+    min_word_count = 3
+    num_workers = multiprocessing.cpu_count()
+    window_size = 5
+    seed = 1
+    downsampling = 1e-3
+    sample = [noun[i,0].split(",") + noun[i,1].split(",") for i in range(num_features)]
+    model = w2v.Word2Vec(sg=1, seed=seed, workers=num_workers, size=num_features, 
+                         min_count=min_word_count, window=window_size, sample=downsampling, 
+                         iter=30)
+    model.build_vocab(sample)
+    print(len(model.wv.vocab))
+    model.train(sample, total_examples=model.corpus_count, epochs=model.epochs)
+    for word in word_list:
+        print("%s's positive similarity: " %(word), model.most_similar(positive=[word]))
+        print("%s's negative similarity: " %(word), model.most_similar(negative=[word]))
+        
+word2vector(j_noun, ['원전', '원자력'])
+word2vector(h_noun, ['원전', '원자력'])
 
-model = w2v.Word2Vec(sg=1, seed=seed, workers=num_workers, size=num_features, min_count=min_word_count,
-               window=window_size, sample=downsampling, iter=1000)
+# matrix #
+#tsne = sklearn.manifold.
 
-model.build_vocab(h_noun[:,1])
-model.train(h_noun[:,1], total_examples=model.corpus_count)
+
+
+
+
